37c59f1525d8996344b55113bd753d9a2d1a0162
Seif El-Din <SeifElDinAyman77@gmail.com>
2023-09-08 03:52:29 +0300

    MSG

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,19 +15,16 @@ chgrp -R ubuntu /data/
 printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
+    add_header X-Served-By $hostname;
     root   /var/www/html;
     index  index.html index.htm;
-
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
-
     location /redirect_me {
-        return 301 http://cuberule.com/;
+        return 301 http://github.com/besthor;
     }
-
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
