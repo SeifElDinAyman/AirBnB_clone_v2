aaa5b2c62badee2201452fa48f04266b62fe9fba
Seif El-Din <SeifElDinAyman77@gmail.com>
2023-09-25 00:36:39 +0300

    MSG

--- web_flask/start_server_with_db.sh
+++ web_flask/start_server_with_db.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db python3 -m web_flask.$1 > /dev/null 2>&1 &
+
+# Ensure the web_flask directory exists
+mkdir -p web_flask
+
+# Set environment variables
+HBNB_MYSQL_USER=hbnb_dev
+HBNB_MYSQL_PWD=hbnb_dev_pwd
+HBNB_MYSQL_HOST=localhost
+HBNB_MYSQL_DB=hbnb_dev_db
+HBNB_TYPE_STORAGE=db
+
+# Run the Flask application
+python3 -m web_flask.$1 > /dev/null 2>&1 &
+
+# Store the process ID (PID) in the .pid file
 echo $! > web_flask/$1.pid
 
-sleep 5;
+sleep 5
+
